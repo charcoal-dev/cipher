87145c69dabc13d2bb66038cce13ad1c696e53d5
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-14 21:48:28 +0500

    Revised file headers in `CipherError` and `CipherException` to use PHPDoc style, simplifying copyright and license references.

--- src/Exception/CipherException.php
+++ src/Exception/CipherException.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/cipher" package.
- * https://github.com/charcoal-dev/cipher
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/cipher/blob/master/LICENSE
+/**
+ * Part of the "charcoal-dev/cipher" package.
+ * @link https://github.com/charcoal-dev/cipher
  */
 
 declare(strict_types=1);
