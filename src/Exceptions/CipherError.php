51f503082390d6b19eec8cdf1490de9f50983f18
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-10-15 00:37:44 +0500

    Update `CipherError` enum to simplify and consolidate error codes

--- src/Exceptions/CipherError.php
+++ src/Exceptions/CipherError.php
@@ -14,14 +14,9 @@ namespace Charcoal\Cipher\Exceptions;
  */
 enum CipherError: int
 {
-    case INVALID_VALUE_TYPE = 0x64;
-    case ENCRYPTION_OP_FAIL = 0xc8;
-    case TAG_REQUIRED = 0x12c;
-    case DECRYPTION_OP_FAIL = 0x190;
-    case RESTORE_SERIALIZED_CONTAINER = 0x1f4;
-    case RETRIEVE_STORED_VALUE_TYPE = 0x258;
-    case INCOMPLETE_SERIALIZED_BUFFER = 0x2bc;
-    case HMAC_COMPUTE_FAIL = 0x320;
-    case PBKDF2_COMPUTE_FAIL = 0x384;
-    case BAD_MASKING_KEY = 0x3e8;
+    case ALGO_KEY_INCOMPATIBLE = 100;
+    case ALGO_UNSUPPORTED = 110;
+    case ENCRYPTION_OP_FAIL = 200;
+    case DECRYPTION_OP_FAIL = 300;
+    case TAG_REQUIRED = 301;
 }
