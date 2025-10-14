c35cb70a1c73a9c1a602ea14dafbf723d71a6199
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-10-15 00:38:27 +0500

    Update dependencies and enhance CipherException documentation
    - Replace `charcoal-dev/base` and `charcoal-dev/buffers` with `charcoal-dev/contracts-security` and `charcoal-dev/secrets-kms`.
    - Refactor `CipherException` class documentation to provide detailed context on cryptographic errors.

--- src/Exceptions/CipherException.php
+++ src/Exceptions/CipherException.php
@@ -9,22 +9,23 @@ declare(strict_types=1);
 namespace Charcoal\Cipher\Exceptions;
 
 /**
- * Class CipherException
- * @package Charcoal\Cipher\Exception
+ * Represents an exception that is specific to cipher operations.
+ *
+ * This exception is used to encapsulate an error related to cryptographic operations,
+ * providing additional context such as a specific error type and optional data for
+ * debugging or further handling.
+ *
+ * @property-read CipherError $error The specific cipher error associated with this exception.
+ * @property-read array $data Additional data providing context about the error.
  */
 class CipherException extends \Exception
 {
-    /**
-     * @param \Charcoal\Cipher\Exceptions\CipherError $error
-     * @param string $message
-     * @param array $data
-     * @param \Throwable|null $previous
-     */
     public function __construct(
         public readonly CipherError $error,
         string                      $message = "",
         public readonly array       $data = [],
-        ?\Throwable                 $previous = null)
+        ?\Throwable                 $previous = null
+    )
     {
         parent::__construct($message, $this->error->value, $previous);
     }
