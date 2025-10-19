113dffbb87017e4301c1a507c91d1c2b397554d5
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-10-20 03:04:17 +0500

    Add `CipherKeyRef` class as a value object for cipher and security key references

---
+++ src/Support/CipherKeyRef.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Part of the "charcoal-dev/cipher" package.
+ * @link https://github.com/charcoal-dev/cipher
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Cipher\Support;
+
+use Charcoal\Cipher\Cipher;
+use Charcoal\Contracts\Security\Secrets\SecretKeyInterface;
+
+/**
+ * Simple VO for cipher algo + security key reference.
+ */
+final readonly class CipherKeyRef
+{
+    public function __construct(
+        public Cipher                    $algo,
+        public string|SecretKeyInterface $kid
+    )
+    {
+    }
+}
